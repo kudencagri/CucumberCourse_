fdeabc36a6286ee6551b78b2408d2a82e57a4b86
kudencagri <Kudencagri@gmail.com>
2025-01-09 12:27:46 +0300

    Jenkins cucumber

--- src/test/java/Runners/_03_TestRunnerAll.java
+++ src/test/java/Runners/_03_TestRunnerAll.java
@@ -6,6 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = {"src/test/java/FeatureFiles"},//klasördeki bütün featurelar
         glue={"StepDefinitions","Utilities"}
+        ,plugin= {"json:target/cucumber/cucumber.json"} // Jenkins için JSON report
 )
 public class _03_TestRunnerAll extends AbstractTestNGCucumberTests {
 }
